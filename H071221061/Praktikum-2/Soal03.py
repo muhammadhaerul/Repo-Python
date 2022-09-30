2c00dc0efe0600bf27ea58d4496f7b209e3f8858
Veyz <114078600+veyzicho@users.noreply.github.com>
2022-09-30 11:23:49 +0800

    Create Soal03.py

---
+++ H071221061/Praktikum-2/Soal03.py
@@ -0,0 +1,10 @@
+a = int(input("Masukan Nilai A: "))
+b = int(input("Masukan Nilai B: "))
+c = int(input("Masukan Nilai C: "))
+
+if a > b and a > c:
+  print('Nilai A:' , a , 'Adalah Yang Terbesar')
+elif b > a and b > c:
+  print('Nilai B:' , b , 'Adalah Yang Terbesar')
+else:
+ print('Nilai C:' , c , 'Adalah Yang Terbesar')
