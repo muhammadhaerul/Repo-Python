0517977beed32dbf9c44fa8a23960371d16ce87a
Minhajul Yusri Khairi <113837592+MinhajulKhairi@users.noreply.github.com>
2022-10-11 09:03:14 +0700

    Update Nomor2.py

--- H071221006/Praktikum-4/Nomor2.py
+++ H071221006/Praktikum-4/Nomor2.py
@@ -10,19 +10,7 @@ while True:
     except:
         print("Input hanya terdiri dari angka")
         continue
-
-# def hitung_fpb(a,b):
-#     if a > b:
-#         terkecil = a
-#     else:
-#         terkecil = b
-#     for i in range(1, terkecil+1):
-#         if (a % i == 0) and (b % i == 0):
-#             fpb = i
-#     return fpb
-# print(f"FPB {(a,b)} = ",hitung_fpb(a,b))
-
-
+        
 def hitung_fpb(a,b):
     while(b):
         a,b = b, a%b
